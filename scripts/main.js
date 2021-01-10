885b922e6db23e49d7f3eea36320d4e6fd2793ed
Brian Thomas <brianjamesthomas@live.com>
2021-01-10 13:08:03 +0000

    Add basic tetrominos

--- scripts/main.js
+++ scripts/main.js
@@ -30,3 +30,52 @@ function drawBoard() {
 }
 
 drawBoard();
+
+const PIECES = [
+    [Z, "red"],
+    [S, "green"],
+    [T, "yellow"],
+    [O, "blue"],
+    [L, "purple"],
+    [I, "cyan"],
+    [J, "orange"],
+];
+
+let p = new Piece(PIECES[0][0], PIECES[0][1]);
+
+function Piece(tetromino, color) {
+    this.tetromino = tetromino;
+    this.color = color;
+    this.tetrominoN = 0;
+    this.activeTetromino = this.tetromino[this.tetrominoN];
+    this.x = 0;
+    this.y = 0;
+}
+
+Piece.prototype.draw = function () {
+    for (r = 0; r < this.activeTetromino.length; r++) {
+        for (c = 0; c < this.activeTetromino.length; c++) {
+            if (this.activeTetromino[r][c]) {
+                drawSquare(this.x + c, this.y + r, this.color);
+            }
+        }
+    }
+};
+
+Piece.prototype.moveDown = function () {
+    this.y++;
+    this.draw();
+};
+
+let dropStart = Date.now();
+function drop() {
+    let now = Date.now();
+    let delta = now - dropStart;
+    if (delta > 1000) {
+        p.moveDown();
+        dropStart = Date.now();
+    }
+    requestAnimationFrame(drop);
+}
+
+drop();
